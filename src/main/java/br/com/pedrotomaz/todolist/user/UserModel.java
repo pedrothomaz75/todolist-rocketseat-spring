bbc645448de239c42b0b91f56bcca8d2d4693db7
pedrothomaz75 <pedroimachida@gmail.com>
2023-10-11 12:28:07 -0300

    feat: fist route created

--- src/main/java/br/com/pedrotomaz/todolist/user/UserModel.java
+++ src/main/java/br/com/pedrotomaz/todolist/user/UserModel.java
@@ -1,7 +1,38 @@
 package br.com.pedrotomaz.todolist.user;
 
 public class UserModel {
-    public String username;
-    public String name;
-    public String password;
+    private String username;
+    private String name;
+    private String password;
+
+    // getters and setters
+      // - getter = mostrar dado
+      // - setter = alterar ou adicionar dado
+
+
+    // Insere valor
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Retorna valor
+    public String getUsername() {
+        return username;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
