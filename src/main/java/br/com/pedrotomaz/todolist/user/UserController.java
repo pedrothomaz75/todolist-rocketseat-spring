b4f96b9c0c267b62d3bc690444b2915dd683b465
pedrothomaz75 <pedroimachida@gmail.com>
2023-10-11 12:12:33 -0300

    feat: fist route created

---
+++ src/main/java/br/com/pedrotomaz/todolist/user/UserController.java
@@ -0,0 +1,25 @@
+package br.com.pedrotomaz.todolist.user;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/*
+*   Modificadores
+*     - public
+*     - private
+*     - protected
+* */
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    // criação do usuário
+    // As informações do usuário irão vir no corpo da requisição
+    // Ou seja, usando o RequestBody
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.name);
+    }
+}
---
+++ src/main/java/br/com/pedrotomaz/todolist/user/UserModel.java
@@ -0,0 +1,7 @@
+package br.com.pedrotomaz.todolist.user;
+
+public class UserModel {
+    public String username;
+    private String name;
+    public String password;
+}
