12cd35e8610d754846b46f38ef6d0fe6858630df
pedrothomaz75 <pedroimachida@gmail.com>
2023-10-12 13:55:06 -0300

    feat: table tasks created

---
+++ src/main/java/br/com/pedrotomaz/todolist/task/TaskModel.java
@@ -0,0 +1,46 @@
+package br.com.pedrotomaz.todolist.task;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+/*
+ *    A tarefa pode ter:
+ *
+ *       - ID
+ *       - Usuário (ID_USUARIO)
+ *       - Descrição
+ *       - Titulo
+ *       - Data de Início
+ *       - Data de Término
+ *       - Prioridade
+ * */
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+
+    // Colocando limite na quantidade de caracteres
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String Priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
