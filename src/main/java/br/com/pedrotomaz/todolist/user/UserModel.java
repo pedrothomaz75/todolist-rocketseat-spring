4d2676cf047af2f454c25f0639dd828f13dbacf7
pedrothomaz75 <pedroimachida@gmail.com>
2023-10-12 11:35:25 -0300

    feat: db created and connection as well

--- src/main/java/br/com/pedrotomaz/todolist/user/UserModel.java
+++ src/main/java/br/com/pedrotomaz/todolist/user/UserModel.java
@@ -1,6 +1,25 @@
 package br.com.pedrotomaz.todolist.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data // inclue os getters and setters
+@Entity(name = "tb_users") // @Entity é a minha tabela no banco de dados e tb_users é o nome dela
 public class UserModel {
+
+    // O meu Id vai ser uma pk que vai usar as funções de UUID
+    @Id
+    // Gera pra mim um valor UUID de forma aleatória
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+
     private String username;
     private String name;
     private String password;
@@ -10,29 +29,9 @@ public class UserModel {
       // - setter = alterar ou adicionar dado
 
 
-    // Insere valor
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    // Retorna valor
-    public String getUsername() {
-        return username;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPassword() {
-        return password;
-    }
+    /*
+    *  Quando os dados forem gerados, o banco vai ter a data e hora de quando isso aconteceu
+    * */
+    @CreationTimestamp
+    private LocalDateTime CreatedAt;
 }
