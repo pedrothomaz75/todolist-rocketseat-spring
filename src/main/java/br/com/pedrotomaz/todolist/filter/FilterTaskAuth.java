911bb8b07c93fd4c456f17b6696f96886dead7ee
pedrothomaz75 <pedroimachida@gmail.com>
2023-10-12 23:51:19 -0300

    feat: route validation

--- src/main/java/br/com/pedrotomaz/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/pedrotomaz/todolist/filter/FilterTaskAuth.java
@@ -23,44 +23,48 @@ public class FilterTaskAuth extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-                // Pega a autenticação e senha
-                var authorization = request.getHeader("Authorization");
-                var authEncoded = authorization.substring("Basic".length()).trim();
 
-                // Decoda o authorization
-                byte[] authDecode = Base64.getDecoder().decode(authEncoded);
-                // Transforma em String
-                var authString = new String(authDecode);
+                // Validando a rota de teste
+                var serveletPath = request.getServletPath();
+                if (serveletPath.equals("/tasks/")) {
 
-                // Separa usuário da senha
-                String[] credentials = authString.split(":");
+                    // Pega a autenticação e senha
+                    var authorization = request.getHeader("Authorization");
+                    var authEncoded = authorization.substring("Basic".length()).trim();
 
-                // Instanciando o username na posição dele no array
-                String username = credentials[0];
-                // Instanciando o password na posição dele no array
-                String password = credentials[1];
+                    // Decoda o authorization
+                    byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+                    // Transforma em String
+                    var authString = new String(authDecode);
 
-                System.out.println("Authorization:");
-                System.out.println(username);
-                System.out.println(password);
+                    // Separa usuário da senha
+                    String[] credentials = authString.split(":");
 
+                    // Instanciando o username na posição dele no array
+                    String username = credentials[0];
+                    // Instanciando o password na posição dele no array
+                    String password = credentials[1];
 
-                // Valida usuário
-                var user = this.userRepository.findByUsername(username);
+                    // Valida usuário
+                    var user = this.userRepository.findByUsername(username);
 
-                // Verificação de existência de usuário
-                if(user == null) {
-                    response.sendError(401);
-                } else {
-                    // valida senha
-                    var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
-                    if(passwordVerify.verified) {
-                        filterChain.doFilter(request, response);
-                    } else {
+                    // Verificação de existência de usuário
+                    if(user == null) {
                         response.sendError(401);
+                    } else {
+                        // valida senha
+                        var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                        if(passwordVerify.verified) {
+                            // Go to Home Page
+                            request.setAttribute("idUSer", user.getId());
+                            filterChain.doFilter(request, response);
+                        } else {
+                            response.sendError(401);
+                        }
                     }
-                    // Entrou na Home Page
 
+                } else {
+                    filterChain.doFilter(request, response);
                 }
     }
 }
--- src/main/java/br/com/pedrotomaz/todolist/task/TaskController.java
+++ src/main/java/br/com/pedrotomaz/todolist/task/TaskController.java
@@ -1,10 +1,13 @@
 package br.com.pedrotomaz.todolist.task;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/tasks")
@@ -14,8 +17,9 @@ public class TaskController {
         private ITaskRepository taskRepository;
 
         @PostMapping("/")
-        public TaskModel create(@RequestBody TaskModel taskModel){
-            System.out.println("Chegou no controller");
+        public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+            var idUSer = request.getAttribute("idUSer");
+            taskModel.setIdUser((UUID) idUSer);
             var task = this.taskRepository.save(taskModel);
             return task;
         }
